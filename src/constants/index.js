6aceecbc24069b82b4711bbc2e177d07fd616c1f
mpromisenuel <mpromisenuel@gmail.com>
2025-04-14 11:46:34 +0000

    Portfolio

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,13 +2,13 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 1/2 years of hands-on experience, I have honed my skills in front-end technologies like React,Vue.js,Nuxt.js and as well as mobile-app technologies like react-native. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 8 years of hands-on experience, I have honed my skills in front-end technologies like React,Vue.js,Nuxt.js and as well as mobile-app technologies like react-native. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile front end developer with a passion for creating efficient and user-friendly web applications. With 1/2 years of professional experience, I have worked with a variety of technologies, including React, Nuxt.js, React-Native and Vue.js. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - 2024",
+    year: "2016 - 2024",
     role: "Frontend Developer", 
     company: "SendAfrika.LMT",
     description: `Designed and developed user interfaces for mobile applications using React-Native. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
